7ebf2b85d5607ccce3493baaed961784ca42a67a
PURWANTO Cedric <57124@2020-57124.local>
2023-02-11 17:18:19 +0900

    WIP setup nickname

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './pages/setup_nickname.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.pink,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const SetupNickNamePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
