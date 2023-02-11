0eefb2d03ed707521da97e115510955d13707054
Issei Mori <isseieikisouya@outlook.jp>
2023-02-11 16:53:20 +0900

    setup info page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pad_project/pages/setup_infomation.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.pink,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: SetUpInformationPage(title: ''),
     );
   }
 }
